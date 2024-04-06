371982ee11d4d262cc839ed2f75042103eb295a6
AbdusamadDev <abdusamaddev571@gmail.com>
2024-04-06 15:42:55 +0500

    Fixed IntegrityError bug

--- accounts/models.py
+++ accounts/models.py
@@ -3,14 +3,17 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 
-
 class Teacher(AbstractUser):
     phone_regex = RegexValidator(
         regex=r"^\+?\d{1,20}$",
         message="Phone number must start with + and must not contain more than 20 digits.",
     )
+    username = None
+    email = None
     phonenumber = models.CharField(
         validators=[phone_regex], max_length=21, unique=True, default="+123456789"
     )
     state = models.BooleanField(default=False)
     fullname = models.CharField(max_length=100, default="...")
+    USERNAME_FIELD = "phonenumber"
+    REQUIRED_FIELDS = []
