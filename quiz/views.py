db911578e5ed4e2587e8fac745365c9096224b6b
AbdusamadDev <abdusamaddev571@gmail.com>
2024-04-06 15:10:50 +0500

    Revert "Prompt authorization for Quiz Creation"
    This reverts commit 238a0d31cbe26e869708391e80acb4a36409c6c8.

--- quiz/views.py
+++ quiz/views.py
@@ -1,14 +1,12 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import CreateAPIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
 
 from .models import Quiz, Question
 from . import serializers
 
 
 class AddQuizAPIView(CreateAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = Quiz.objects.all()
     serializer_class = serializers.QuizSerializer
     
