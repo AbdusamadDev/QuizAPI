238a0d31cbe26e869708391e80acb4a36409c6c8
AbdusamadDev <abdusamaddev571@gmail.com>
2024-04-06 13:30:05 +0500

    Prompt authorization for Quiz Creation

--- quiz/views.py
+++ quiz/views.py
@@ -1,12 +1,14 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import CreateAPIView
-from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Quiz, Question
 from . import serializers
 
 
 class AddQuizAPIView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Quiz.objects.all()
     serializer_class = serializers.QuizSerializer
     
