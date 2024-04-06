c54db0e3b94e8ba173f8495be5f759ae2936cf52
AbdusamadDev <abdusamaddev571@gmail.com>
2024-04-06 12:42:17 +0500

    Authentication [testing]

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import authentication_classes, api_view, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(http_method_names=["GET"])
+# @authentication_classes(authentication_classes=[TokenAuthentication])
+@permission_classes(permission_classes=[IsAuthenticated])
+def testing(request):
+    return Response({"data": "testing"})
