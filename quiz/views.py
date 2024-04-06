baf891c1f2dafb42953c06aff5b5e4252ad58c12
Winchestor2001 <sbexruz2001@gmail.com>
2024-04-06 12:43:18 +0500

    add endpoint for add quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Quiz, Question
+from . import serializers
+
+
+class AddQuizAPIView(CreateAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = serializers.QuizSerializer
+    
