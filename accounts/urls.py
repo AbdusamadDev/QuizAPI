2549ea1c480c6d190646d82ea71678ce5612e17b
AbdusamadDev <abdusamaddev571@gmail.com>
2024-04-06 15:26:03 +0500

    Teacher registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import TeacherAuthViewSet
+from .views import TeacherRegistrationAPIView, test
 
 
 urlpatterns = [
@@ -14,16 +14,17 @@ urlpatterns = [
     path(
         "token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
     ),  # bu birinchi marta avtorizatsiya qilganda beriladigan access & refresh token
-    path("register/", TeacherAuthViewSet.as_view({"get": "list", "post": "create"})),
-    path(
-        "teachers/<int:pk>/",
-        TeacherAuthViewSet.as_view(
-            {
-                "get": "details",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy",
-            }
-        ),
-    ),
+    path("register/", TeacherRegistrationAPIView.as_view()),
+    path("test/", test),
+    # path(
+    #     "teachers/<int:pk>/",
+    #     TeacherAuthViewSet.as_view(
+    #         {
+    #             "get": "details",
+    #             "put": "update",
+    #             "patch": "partial_update",
+    #             "delete": "destroy",
+    #         }
+    #     ),
+    # ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,35 +1,42 @@
-from django.contrib.auth.hashers import make_password
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView
+from rest_framework import generics, status
 from rest_framework.response import Response
-from django.db import IntegrityError
-from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import TeacherSerializer
 from .models import Teacher
 
 
-class TeacherAuthViewSet(ModelViewSet):
+class TeacherRegistrationAPIView(CreateAPIView):
+    queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
-    model = Teacher
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-            validated_data = serializer.validated_data
-            password = validated_data.pop("password", None)
-            if password:
-                validated_data["password"] = make_password(
-                    password
-                )  # Encrypt the password
-            self.perform_create(serializer)
-        except IntegrityError:
+        if serializer.is_valid():
+            teacher = serializer.save()
+            teacher.set_password(request.data.get("password"))
+            teacher.save()
+
+            # Generating JWT token upon successful creation
+            refresh = RefreshToken.for_user(teacher)
+            token_data = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
             return Response(
-                {"error": "Teacher with this phone number or username already exists."},
-                status=status.HTTP_400_BAD_REQUEST,
+                {"tokens": token_data},
+                status=status.HTTP_201_CREATED,
             )
-        headers = self.get_success_headers(serializer.data)
-        response_data = {
-            "data": "Teacher Creation succeeded, you may obtain an authorization token!"
-        }
-        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def test(request):
+    return Response({"data": "messdfsdf"})
+
+
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzEyNDAyNTYyLCJpYXQiOjE3MTIzOTg5NjIsImp0aSI6IjI3OTNkNzExNjQxMzQyNmZhM2RhZWQ5OWEyNzY0ZWNiIiwidXNlcl9pZCI6MX0.3zm-j8pG778ULEOi-CnvsmqkvOaMEey_s3sknPPTUbw
