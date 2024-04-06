a284621ef926492707c9ab4cb8e095532cc6f1e5
Winchestor2001 <sbexruz2001@gmail.com>
2024-04-06 16:01:03 +0500

    fix bug in accounts app and add export question endpoint

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('add/', views.AddQuizAPIView.as_view()),
-    path('add_questions/', views.AddQuestionAPIView.as_view()),
+    path('add_question/', views.AddQuestionAPIView.as_view()),
     path('quizzes_list/', views.QuizAPIView.as_view()),
     path('edit/', views.QuizAPIView.as_view()),
-    path('edit_questions/', views.QuestionAPIView.as_view()),
+    path('edit_question/', views.QuestionAPIView.as_view()),
+    path('export_questions/', views.ExportQuestionAPIView.as_view()),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,7 @@
 
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Quiz, Question
@@ -22,14 +23,20 @@ class QuizAPIView(generics.ListAPIView, generics.UpdateAPIView):
 
 class AddQuestionAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
-    queryset = Quiz.objects.all()
+    queryset = Question.objects.all()
     serializer_class = serializers.QuestionSerializer
 
 
 class QuestionAPIView(generics.ListAPIView, generics.UpdateAPIView):
     permission_classes = [IsAuthenticated]
-    queryset = Quiz.objects.all()
+    queryset = Question.objects.all()
     serializer_class = serializers.QuestionSerializer
     lookup_field = 'id'
 
+
+class ExportQuestionAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        pass
     
